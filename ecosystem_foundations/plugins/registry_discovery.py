7835f445777623a0e09b5871a4060bac2c141ee0
Laurkan <laurkanr@gmail.com>
2026-07-23 11:21:42 -0400

    added better registry discovery and an __init__.py file

--- ecosystem_foundations/plugins/registry_discovery.py
+++ ecosystem_foundations/plugins/registry_discovery.py
@@ -4,7 +4,12 @@ from importlib import import_module
 
 def autodiscover_registries():
     for app_config in apps.get_app_configs():
+        module_path = f"{app_config.name}.registry"
+
         try:
-            import_module(f"{app_config.name}.registry")
-        except ModuleNotFoundError:
-            continue
+            import_module(module_path)
+
+        except ModuleNotFoundError as e:
+            # Only ignore if the registry module itself is missing
+            if e.name != module_path:
+                raise
