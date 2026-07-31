04c45c58e2c2eb3dfc5c195481ed821235a9ec5a
Laurkan <laurkanr@gmail.com>
2026-07-31 18:18:57 -0400

    added some admin registration

--- ecosystem_foundations/automation/admin.py
+++ ecosystem_foundations/automation/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import AutomationAction, AutomationActionRun, AutomationRule, AutomationRun, AutomationTrigger
+
 # Register your models here.
+admin.register(AutomationRule)
+admin.register(AutomationTrigger)
+admin.register(AutomationAction)
+admin.register(AutomationRun)
+admin.register(AutomationActionRun)
--- ecosystem_foundations/watchdog/admin.py
+++ ecosystem_foundations/watchdog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Signal, SignalItemType
+
 # Register your models here.
+admin.register(Signal)
+admin.register(SignalItemType)
