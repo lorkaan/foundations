768c58ab350ecae09526e3c4d1513e38918a9669
Laurkan <laurkanr@gmail.com>
2026-08-22 10:39:43 -0400

    added the csrf fix

--- ecosystem_foundations/iam/views.py
+++ ecosystem_foundations/iam/views.py
@@ -7,15 +7,16 @@ from ..base.views import BaseQueryViewSetMixin, ContentTypeQuerysetMixin, Foreig
 from ..users.serializers import UserSerializer
 
 from rest_framework import status
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+@method_decorator(ensure_csrf_cookie, name="dispatch")
 class CsrfView(APIView):
     permission_classes = [AllowAny]
 
-    @ensure_csrf_cookie
     def get(self, request):
         return Response({
             "detail": "CSRF cookie set"
